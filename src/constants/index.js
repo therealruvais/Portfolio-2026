b6650b1af66079a33eed1b61c4096b7ba8b6ff0b
therealruvais <ruvais666@gmail.com>
2026-03-18 12:31:17 +0530

    New commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -135,7 +135,7 @@ export const myProjects = [
 export const mySocials = [
   {
     name: "WhatsApp",
-    href: "",
+    href: "https://wa.me/7012768216",
     icon: "/assets/socials/whatsApp.svg",
   },
   {
